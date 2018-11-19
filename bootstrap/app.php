cc54999e757c9964127bb22f375f31d0a462c8eb
ralphjohn29 <ralphjohn292@gmail.com>
2018-11-20 06:05:37 +0800

    Generate question model

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,6 +25,7 @@ $app = new Illuminate\Foundation\Application(
 | incoming requests to this application from both the web and CLI.
 |
 */
+$app->register(Sayeed\CustomMigrate\CustomMigrateServiceProvider::class);
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
