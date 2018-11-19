c987206b3f82e5502ee8339f8d5cbdc6288f5194
ralphjohn29 <ralphjohn292@gmail.com>
2018-11-20 04:36:35 +0800

    Initial Commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,30 +1,29 @@
 <?php
 
 namespace App\Providers;
-use Illuminate\Support\Facades\Schema; //Import Schema
-
+use Illuminate\Support\Facades\Schema;
 
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        Schema::defaultStringLength(191); //Solved by increasing StringLength
-    }
+/**
+ * Bootstrap any application services.
+ *
+ * @return void
+ */
+public function boot()
+{
+Schema::defaultStringLength(191); //Solved by increasing StringLength
+}
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+/**
+ * Register any application services.
+ *
+ * @return void
+ */
+public function register()
+{
+    //
+}
 }
